6aad45df4a919d4700419b6eeeeb694de4046ab2
sinfulNO <60526757+sinfulNO@users.noreply.github.com>
2021-11-04 15:17:15 -0600

    Fine

--- .vscode/cquery_cached_index/c@@users@spots@documents@github@arrowhead-best-2021/src@opcontrol.c
+++ .vscode/cquery_cached_index/c@@users@spots@documents@github@arrowhead-best-2021/src@opcontrol.c
@@ -43,12 +43,5 @@ void operatorControl() {
         motorSet(2, -power - turn); // set left wheels
         motorSet(3, -power + turn); // set right wheels
         delay(20);
-
-        if(joystickGetDigital(1, 8, JOY_UP)) {
-          motorSet(4, 127);
-        }
-        else{
-          motorSet(4, -127);
-        }
     }
 }
